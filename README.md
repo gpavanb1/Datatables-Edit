bf9f9cbf6cb48ec96886e8fc0cf4d3ad0e9246d8
Pavan BG <gpbabc@yahoo.com>
2019-12-01 20:34:19 +0530

    working

---
+++ README.md
@@ -0,0 +1,29 @@
+# Datatables-Edit
+
+![Made with Love in India](https://madewithlove.org.in/badge.svg)
+
+This repository contains HTML+CSS+JS code
+using Datatables for Bootstrap and extending
+it to include add/edit/delete options.
+
+The table has following features demonstrated
+* search
+* pagination
+* scroll
+* export to CSV, PDF, Excel
+* send to printer
+* add
+* edit
+* delete
+
+## How to install and execute
+Open `index.html`
+
+## Sample Output
+
+![sample](images/sample.png)
+
+## Whom to contact?
+
+Please direct your queries to [gpavanb1](http://github.com/gpavanb1)
+for any questions.
---
+++ js/index.js
@@ -0,0 +1,85 @@
+const $tableID = $('#table');
+const $BTN = $('#export-btn');
+const $EXPORT = $('#export');
+var index = 0;
+
+$(document).ready(function () {
+  $('[data-toggle="tooltip"]').tooltip({
+        boundary: "window",
+        container: "body"
+    }); 
+  var actions = `<a class="add" title="Confirm" data-toggle="tooltip"><i class="fa fa-check"></i></a>
+                 <a class="edit" title="Edit" data-toggle="tooltip"><i class="fas fa-edit"></i></a>
+                 <a class="delete" title="Delete" data-toggle="tooltip"><i class="fa fa-trash"></i></a>`;
+  var dataTable = $('#dtBasic').DataTable({
+    dom: 'lfrtBip',
+    "paging": true, // false to disable pagination (or any other option)
+    "ordering": false, //disable ordering
+    "scrollY": "300px",
+    "scrollCollapse": true,
+    buttons: [
+        'copy', 'csv', 'excel', 'pdf', 'print'
+    ]
+  });
+  dataTable.draw();
+  
+  $('.table-add').click(function() {
+    $(this).attr("disabled", "disabled");
+    var row = '<tr class="hide">'+
+    '<td><input type="text" class="form-control" name="name" id="name"></td>' +
+    '<td><input type="text" class="form-control" name="department" id="department"></td>' +
+    '<td><input type="text" class="form-control" name="phone" id="phone"></td>' +
+    '<td><input type="text" class="form-control" name="name" id="name"></td>' +
+    '<td><input type="text" class="form-control" name="department" id="department"></td>' +
+    '<td>' + actions + '</td>' +
+    '</tr>';
+    dataTable.row.add($(row)).draw();
+    $("table tbody tr").eq(index).find(".add, .edit").toggle();
+    index += 1;
+    $('[data-toggle="tooltip"]').tooltip({
+        boundary: "window",
+        container: "body"
+    });;
+  });
+  
+    // Add row on add button click
+    $(document).on("click", ".add", function(){
+        var empty = false;
+        var input = $(this).parents("tr").find('input[type="text"]');
+        input.each(function(){
+            if(!$(this).val()){
+                $(this).addClass("error");
+                empty = true;
+            } else{
+                $(this).removeClass("error");
+            }
+        });
+        $(this).parents("tr").find(".error").first().focus();
+        if(!empty){
+            input.each(function(){
+                $(this).parent("td").html($(this).val());
+            });
+            $(this).parents("tr").find(".add, .edit").toggle();
+            $(".add-new").removeAttr("disabled");
+        }
+    });
+
+    // Edit row on edit button click
+    $(document).on("click", ".edit", function(){
+        $(this).parents("tr").find("td:not(:last-child)").each(function(){
+            $(this).html('<input type="text" class="form-control" value="' + $(this).text() + '">');
+        });		
+        $(this).parents("tr").find(".add, .edit").toggle();
+        $(".add-new").attr("disabled", "disabled");
+    });
+
+    // Delete row on delete button click
+    $(document).on("click", ".delete", function(){
+        $(this).tooltip('hide');
+        dataTable.row($(this).parents("tr")).remove().draw();
+        $(".add-new").removeAttr("disabled");
+        index -= 1;
+    });
+
+});
+ 
